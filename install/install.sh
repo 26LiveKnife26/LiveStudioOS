53f217c1880c58b9038b15bb40c736b0ee63c98f
PythonIsGo <kirill26092014@gmail.com>
2024-11-12 17:00:47 +1000

    Beta LiveStudioOS 0.1

---
+++ install/install.sh
@@ -0,0 +1,23 @@
+echo THIS INSTALLISATION GUIDE FOR DEBIAN/UBUNTU/LINUX MINT
+sudo apt update
+sudo apt upgrade
+sudo apt install python3
+sudo apt install python3-pip
+echo INSTALLING PIP3-PACKAGES FOR OS
+pip install -r requirements.txt
+echo PY-VER = %PY_VER
+echo PY-VER = /new_line
+python3 --version
+cd ..
+echo INSTALLISATION COMPLECTED SUCEFFULY
+echo per = 3.10 - 3.12
+echo liveboot...
+echo liveboot..
+echo per = 3
+echo mv python3 super/user/username/python/
+echo mv per_os super/python/
+echo else{
+echo mv error_fatal super/user/fatals/
+echo }
+echo complected
+echo REBOOT YOUR SYSTEM PLEASE
